d926588d664e1b93903747a22510d5ad89e0d2ac
Đinh Đức Thông <ddthht@gmail.com>
2018-04-25 09:02:25 +0700

    Push all

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,23 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { ChampionsComponent } from './champions/champions.component';
+import { ItemsComponent } from './items/items.component';
+import { ChampionDetailComponent } from './champion-detail/champion-detail.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavBarComponent,
+    ChampionsComponent,
+    ItemsComponent,
+    ChampionDetailComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
